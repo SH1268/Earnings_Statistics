e3dda7060550cd23805af1ab985b3f714e86cdaf
Marenas2001 <ma1956@georgetown.edu>
2022-12-07 18:15:09 -0500

    Creating Deploying.md and init file

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 import os
- from dotenv import load_dotenv
+from dotenv import load_dotenv
 
- load_dotenv()
+load_dotenv()
 
- APP_ENV = os.getenv("APP_ENV", default="development")
+APP_ENV = os.getenv("APP_ENV", default="development")
