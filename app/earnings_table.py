66cbc5e3099d529e517b5156df910e957d2635b0
SH1268 <115600478+SH1268@users.noreply.github.com>
2022-12-07 17:00:13 -0500

    Refactored to earnings.py and earnings_table.py

--- app/earnings_table.py
+++ app/earnings_table.py
@@ -1,9 +1,8 @@
 import json
 from pprint import pprint
-
+import pandas
 
 import requests
-from dotenv import load_dotenv
 
 from app.alpha import API_KEY
 
@@ -31,19 +30,18 @@ def fetch_quarterly_earnings_data():
         return parsed_response["quarterlyEarnings"]
 
 if __name__ == "__main__":
+        print("Earnings Report")
+         
+        data_type = input("Would you like to look at annual data or quarterly data?: ") or "annual"
 
-        data_selected = input("Would you like to look at annual data or quarterly data?: ") or "annual"
-
-        annual = "annualEarnings"
-        quarterly = "quarterlyEarnings"
-
-        if data_selected == "annual":
+        if data_type == "annual":
                 data = fetch_annual_earnings_data()
-        elif data_selected == "quarterly": 
-                data = fetch_quarterly_earnings_data
+                new_data = pandas.DataFrame(data)
+                print(new_data)
 
-        data = fetch_annual_earnings_data()
-        new_data = pandas.DataFrame(data)
-        print(new_data)
+        elif data_type == "quarterly":
+                data = fetch_quarterly_earnings_data()
+                new_data = pandas.DataFrame(data)
+                print(new_data)
 
         
