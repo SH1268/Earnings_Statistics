43e8b6c5c14c9041e96dd9f215503b67a3346649
SH1268 <115600478+SH1268@users.noreply.github.com>
2022-12-07 12:21:27 -0500

    reconfiguring earnings.py file

--- app/earnings.py
+++ app/earnings.py
@@ -4,27 +4,32 @@ import json
 from pprint import pprint
 
 import requests
+from app.alpha import API_KEY
 
-API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
+symbol = input("Please enter a symbol (default = IBM): ") or "IBM"
+print("SYMBOL: ", symbol)
 
-request_url = f"https://www.alphavantage.co/query?function=EARNINGS&symbol=IBM&apikey={API_KEY}"
+def fetch_earnings_data():
+        request_url = f"https://www.alphavantage.co/query?function=EARNINGS&symbol={symbol}&apikey={API_KEY}"
+        response = requests.get(request_url)
+        parsed_response = json.loads(response.text)
 
-response = requests.get(request_url)
 
-parsed_response = json.loads(response.text)
-#print(type(parsed_response))
-#pprint(parsed_response)
+if __name__ == "__main__":
+        print("Earnings Report")
 
-annual = "annualEarnings"
-quarterly = "quarterlyEarnings"
-
-data_selected = input("Would you like to look at annual data or quarterly data?: ") or "annual"
-
-if data_selected == "annual":
-     data_selected = annual
-elif data_selected == "quarterly": 
-    data_selected = quarterly
-
-data = parsed_response[data_selected]
-
-date_selected = input("Please enter a year (default = 2022): ") or "2022"
+        data = fetch_earnings_data
+        
+        annual = "annualEarnings"
+        quarterly = "quarterlyEarnings"
+        
+        data_selected = input("Would you like to look at annual data or quarterly data?: ") or "annual"
+        
+        if data_selected == "annual":
+                data_selected = annual
+        elif data_selected == "quarterly": 
+                data_selected = quarterly
+                
+        data[data_selected]
+        
+        date_selected = input("Please enter a year (default = 2022): ") or "2022"
