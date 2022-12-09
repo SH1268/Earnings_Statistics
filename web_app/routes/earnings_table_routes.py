2a6c53d81fea275ef5dbfe7dfecb4264c0e048ac
SH1268 <115600478+SH1268@users.noreply.github.com>
2022-12-09 11:42:49 -0500

    Checkpoint 3: Fixed HTML for Graph and Table Dashboard

--- web_app/routes/earnings_table_routes.py
+++ web_app/routes/earnings_table_routes.py
@@ -6,10 +6,15 @@ from app.earnings_table import fetch_annual_earnings_data, fetch_quarterly_earni
     
 earnings_table_routes = Blueprint("earnings_table_routes", __name__)
     
-@earnings_table_routes.route("/earnings_table/form")
-def earnings_table_form():
-        print("EARNINGS FORM...")
-        return render_template("earnings_table_form.html")
+@earnings_table_routes.route("/earnings_table_annual/form")
+def annual_earnings_table_form():
+        print("A EARNINGS FORM...")
+        return render_template("earnings_table_annual_form.html")
+
+@earnings_table_routes.route("/earnings_table_quarterly/form")
+def quarterly_earnings_table_form():
+        print("Q EARNINGS FORM...")
+        return render_template("earnings_table_quarterly_form.html")
     
 @earnings_table_routes.route("/earnings_table/annual_dashboard",  methods=["GET", "POST"])
 def earnings_table_annual_dashboard():
@@ -33,7 +38,7 @@ def earnings_table_annual_dashboard():
             latest_date = latest["fiscalDateEnding"]
 
             #flash("Fetched Real-time Market Data!", "success")
-            return render_template("earnings_table.html",
+            return render_template("earnings_table_annual.html",
                 symbol=symbol,
                 latest_eps=latest_eps,
                 latest_date=latest_date,
@@ -43,7 +48,41 @@ def earnings_table_annual_dashboard():
             print('OOPS', err)
     
             #flash("Market Data Error. Please check your symbol and try again!", "danger")
-            return redirect("/earnings_table/form")
+            return redirect("/earnings_table_annual/form")
+
+@earnings_table_routes.route("/earnings_table/quarterly_dashboard",  methods=["GET", "POST"])
+def earnings_table_quarterly_dashboard():
+        print("EARNINGS DASHBOARD...")
+    
+        if request.method == "POST":
+            # for data sent via POST request, form inputs are in request.form:
+            request_data = dict(request.form)
+            print("FORM DATA:", request_data)
+        else:
+            # for data sent via GET request, url params are in request.args
+            request_data = dict(request.args)
+            print("URL PARAMS:", request_data)
+    
+        symbol = request_data.get("symbol") or "IBM"
+    
+        try:
+            data = fetch_quarterly_earnings_data(symbol=symbol)
+            latest = data[0]
+            latest_eps = float(latest["reportedEPS"])
+            latest_date = latest["fiscalDateEnding"]
+
+            #flash("Fetched Real-time Market Data!", "success")
+            return render_template("earnings_table_quarterly.html",
+                symbol=symbol,
+                latest_eps=latest_eps,
+                latest_date=latest_date,
+                data=data
+            )
+        except Exception as err:
+            print('OOPS', err)
+    
+            #flash("Market Data Error. Please check your symbol and try again!", "danger")
+            return redirect("/earnings_table_quarterly/form")
     
     #
     # API ROUTES
